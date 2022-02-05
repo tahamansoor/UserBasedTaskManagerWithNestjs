2dac06e6127a79306524882a1e62ac04bf996c57
Mubeen Alam Usmani <mubeenalam5@gmail.com>
2022-02-05 13:24:56 +0500

    some changes

--- src/tasks/config/typeorm.config.ts
+++ src/tasks/config/typeorm.config.ts
@@ -1,16 +1,15 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 import { User } from "src/auth/user.entity";
 import { task } from "../task.entity";
-
 export const typeOrmConfig: TypeOrmModuleOptions = {
-    type: 'mysql',
-    host:process.env.HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT) || 3306,
-    database: process.env.DB_NAME || 'testtask',
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || '',
-    entities: [User,task],
-    synchronize: true
-//    
-
-}
+    // 
+        type: 'postgres',
+        host:process.env.HOST,
+        port: parseInt(process.env.DB_PORT),
+        database: process.env.DB_NAME,
+        username: process.env.DB_USER ,
+        password: process.env.DB_PASS ,    
+        entities: [User,task],
+        synchronize: true
+    }
+    console.log(typeOrmConfig)
